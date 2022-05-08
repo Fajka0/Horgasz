2ca03ad76abe0ecf23380da6d2a3d1dc95aa83ef
Fajka0 <fajkaviki@gmail.com>
2022-05-08 21:28:30 +0200

    Kesz

---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,182 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
+    private static final int SECRET_KEY = 49;
+
+    EditText ETuserName;
+    EditText ETuserEmail;
+    EditText ETpassword;
+    EditText ETpasswordConfirm;
+    EditText ETphone;
+    Spinner spinner;
+    EditText ETadress;
+    RadioGroup accountTypeGroup;
+
+    private SharedPreferences preferences;
+    private FirebaseAuth mAuth;
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        super.addContentView(view, params);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        int secret_key=getIntent().getIntExtra("SECRET_KEY",0);
+        if(secret_key!=49){
+            finish();
+        }
+
+        ETuserName = findViewById(R.id.userNameEditText);
+        ETuserEmail = findViewById(R.id.userEmailEditText);
+        ETpassword = findViewById(R.id.passwordEditText);
+        ETpasswordConfirm = findViewById(R.id.passwordAgainEditText);
+        ETphone = findViewById(R.id.phoneEditText);
+        ETadress = findViewById(R.id.addressEditText);
+        spinner = findViewById(R.id.phoneSpinner);
+        accountTypeGroup = findViewById(R.id.accountTypeGroup);
+        accountTypeGroup.check(R.id.buyer);
+
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        ETuserName.setText(userName);
+        ETpassword.setText(password);
+        ETpasswordConfirm.setText(password);
+
+
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.phone_labels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        Log.i(LOG_TAG, "onCreate");
+
+    }
+
+    public void register(View view) {
+        String userName = ETuserName.getText().toString();
+        String email = ETuserEmail.getText().toString();
+        String password = ETpassword.getText().toString();
+        String passwordConfirm = ETpasswordConfirm.getText().toString();
+        if (!password.equals(passwordConfirm)) {
+            Log.e(LOG_TAG, "Nem egyenlő a jelszó és a megerősítése.");
+            return;
+        }
+        String phone = ETphone.getText().toString();
+        String phoneType = spinner.getSelectedItem().toString();
+        String adress = ETadress.getText().toString();
+
+        int accountTypeId = accountTypeGroup.getCheckedRadioButtonId();
+        View radioButton = accountTypeGroup.findViewById(accountTypeId);
+        int id = accountTypeGroup.indexOfChild(radioButton);
+        String accountType =  ((RadioButton)accountTypeGroup.getChildAt(id)).getText().toString();
+
+
+        //Log.i(LOG_TAG, "Regisztrált: " + userName + ", e-mail: " + email);
+       // startShopping();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d(LOG_TAG, "User created successfully");
+                    startShopping();
+                } else {
+                    Log.d(LOG_TAG, "User was't created successfully:", task.getException());
+                    Toast.makeText(RegisterActivity.this, "User was't created successfully:", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+
+    private void startShopping(/* registered used class */) {
+        Intent intent = new Intent(this, ShopListActivity.class);
+       // intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String selectedItem = adapterView.getItemAtPosition(position).toString();
+        Log.i(LOG_TAG, selectedItem);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
